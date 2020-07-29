99bacb26fcd224e9766b7b27daa7375b7d32faa4
yichunzhao <zhaoyichun@hotmail.com>
2020-07-29 13:06:17 +0200

    constrain DI by qualifier

--- src/main/java/com/ynz/democonfigproperties/aircraft/AirCraftController.java
+++ src/main/java/com/ynz/democonfigproperties/aircraft/AirCraftController.java
@@ -1,6 +1,7 @@
 package com.ynz.democonfigproperties.aircraft;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("aircraft")
 @RequiredArgsConstructor
 public class AirCraftController {
+    @Qualifier("f15")
     private final JetFighter f15;
+
+    @Qualifier("f16")
     private final JetFighter f16;
-    private final JetFighter x;
 
     @GetMapping("f15")
     public JetFighter getF15() {
@@ -21,10 +24,5 @@ public class AirCraftController {
     @GetMapping("f16")
     public JetFighter getF16() {
         return f16;
-    }
-
-    @GetMapping("x")
-    public JetFighter getX() {
-        return x;
     }
 }

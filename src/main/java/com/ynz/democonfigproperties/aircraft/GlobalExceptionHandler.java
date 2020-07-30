41f8cb9a99a6ef5f1d1eb049b61e3b0e966b9c55
yichunzhao <zhaoyichun@hotmail.com>
2020-07-30 05:10:20 +0200

    removing packages

--- src/main/java/com/ynz/democonfigproperties/aircraft/GlobalExceptionHandler.java
+++ src/main/java/com/ynz/democonfigproperties/aircraft/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.ynz.democonfigproperties.aircraft.exception;
+package com.ynz.democonfigproperties.aircraft;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,7 +12,7 @@ import static java.util.stream.Collectors.toSet;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(ConstraintViolationException.class)
+    @ExceptionHandler({ConstraintViolationException.class})
     public ResponseEntity<Set<String>> handleConstraintViolationException(ConstraintViolationException  exception){
         return ResponseEntity.badRequest()
                 .body(exception.getConstraintViolations().stream().map(x->x.getMessage()).collect(toSet()));
